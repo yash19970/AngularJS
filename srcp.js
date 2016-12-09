38270331d6d3b771c961541cb766365903c37126
yash19970 <yash_19@outlook.com>
2016-12-09 11:37:23 +0530

    events completed in angular.

--- srcp.js
+++ srcp.js
@@ -4,7 +4,8 @@
 											var country = {
 											name : "USA",
 											cap: "ees",
-											flag 	: "Images/usa-flag.png" 	};
+											flag 	: "Images/usa-flag.png" 	
+										};
 											$scope.country = country;
 										});
  myImg.controller("myController",function ($scope){
@@ -14,5 +15,28 @@
  		age : 	"10"
  	}
  	$scope.emp = emp;
- });
- 
+					});
+
+myImg.controller("arrayController",function ($scope){
+										 	var employees = [
+										 		{fname : "David", lname : "jason" , salary : "54500" },
+										 		{fname : "David", lname : "jason" , salary : "54500" },
+										 		{fname : "David", lname : "jason" , salary : "54500" }
+										 	];
+										 	$scope.employees = employees;
+											});
+myImg.controller("eventController",function($scope){
+	var tech = [
+	{ name: "PHP", likes:0,dislikes:0},
+	{ name: "Python", likes:0,dislikes:0},
+	{ name: "Angularjs", likes:0,dislikes:0},
+
+	];
+	$scope.tech = tech;
+	$scope.incLikes = function(techology){
+		techology.likes++;
+	}
+	$scope.desLikes = function(techology){
+		techology.dislikes++;
+	}
+});
