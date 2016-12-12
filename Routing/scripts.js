7e97f5a8d0a85a430e74b096369d31bcb8927ac4
yash19970 <yash_19@outlook.com>
2016-12-12 17:19:19 +0530

    Routing completed

--- Routing/scripts.js
+++ Routing/scripts.js
@@ -2,21 +2,29 @@
  				.module("demo",["ngRoute"])
  				.config(function ($routeProvider){
  				 					$routeProvider
- 				 					.when("/",{
+ 				 					/*.when("/",{
  				 						templateUrl :'Template/trial.html',
  				 						 controller :'homeController'
- 				 					})
+ 				 					})*/
  				 					.when("/home",{
- 				 						templateUrl :'Template/home.html',
- 				 						 controller :'homeController'
+ 				 						 templateUrl :'Template/home.html',
+ 				 						 controller :'homeController',
+ 				 						 caseInsensitiveMatch : 'true'
  				 					})
  				 					.when("/courses",{
- 				 						templateUrl : "Template/courses.html",
- 				 						 controller 	: "coursesController"
+ 				 						 templateUrl : "Template/courses.html",
+ 				 						 controller 	: "coursesController",
+ 				 						 caseInsensitiveMatch : 'true'
  				 					})
  				 					.when("/students",{
- 				 						templateUrl : "Template/students.html",
- 				 						 controller : "studentsController"
+ 				 						 templateUrl : "Template/students.html",
+ 				 						 controller : "studentsController",
+ 				 						 caseInsensitiveMatch : 'true'
+ 				 					})
+ 				 					.when("/students/:id",{
+ 				 						templateUrl     : "Template/studentdetails.html",
+ 				 						 controller 	: "studentDetailsController",
+ 				 						 caseInsensitiveMatch : 'true'
  				 					})
  				 					.otherwise({
  				 						redirectTo : "/home"
@@ -28,6 +36,16 @@ myApp.controller("homeController",function($scope){
 myApp.controller("coursesController",function($scope){
 											$scope.courses = ["C#","PHP","ANGULAR"];
 										});
+myApp.controller("studentDetailsController",function($scope,$http,$routeParams){
+										$http({
+											url : "http://localhost/angularproject/index.php/Angularapi/viewallbyid",
+											params: { id:$routeParams.id },
+											method : "get",
+										})
+										.then(function(response){
+											$scope.student = response.data; 
+										})
+});
 myApp.controller("studentsController",function($scope,$http){
  											$http.get('http://localhost/angularproject/index.php/Angularapi/viewall')
  												 .then(function(response){
